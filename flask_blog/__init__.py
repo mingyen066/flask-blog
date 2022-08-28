a1798440fabd5285b1fcdb7c6783ea5e8f4e6569
Ming-Yen Chung <mingyen_chung@trendmicro.com>
2022-08-29 00:00:45 +0800

    add login function

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,11 +2,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9deaa968eb466a4c196c8e60dc6da5f4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flask_blog import routes
